44777aee8e63d8e883d6ff98fc52e0bda59b15f7
soihuy2007-tech <soihuy2007@gmail.com>
2026-03-27 14:03:53 +0700

    Add files via upload

--- lab3.py
+++ lab3.py
@@ -3,21 +3,36 @@ import sqlite3
 conn = sqlite3.connect("lab3.db")
 cursor = conn.cursor()
 
+# Xóa bảng cũ nếu tồn tại
+cursor.execute("DROP TABLE IF EXISTS khach_hang")
+cursor.execute("DROP TABLE IF EXISTS san_pham")
+
+# Tạo bảng khách hàng
 cursor.execute("""
 CREATE TABLE khach_hang (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    ten TEXT,
-    tuoi INTEGER,
+    ma_khach_hang INTEGER PRIMARY KEY,
+    ho_ten TEXT,
+    so_dien_thoai TEXT,
     dia_chi TEXT
 )
 """)
 
+# Tạo bảng sản phẩm
 cursor.execute("""
-INSERT INTO khach_hang (ten, tuoi, dia_chi)
-VALUES ('Nguyen Van A', 20, 'Ha Noi')
+CREATE TABLE san_pham (
+    ma_san_pham INTEGER PRIMARY KEY,
+    ten_san_pham TEXT,
+    gia_tien REAL
+)
+""")
+
+# Thêm dữ liệu mẫu
+cursor.execute("""
+INSERT INTO khach_hang VALUES
+(1, 'Nguyen Van Anh', '0123456789', 'Ha Noi')
 """)
 
 conn.commit()
 conn.close()
 
-print("Tao bang thanh cong!")
+print("Chạy OK!")
